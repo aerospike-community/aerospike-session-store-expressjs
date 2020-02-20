0f50c4a3fe9df88d083ba0fadf215af41d71e821
Jan Hecking <jhecking@aerospike.com>
2020-02-20 10:55:31 +0800

    Fix Travis CI tests (#14)
    * Update Travis & Aerospike Server conf
    * Fix async tests: Wait for client to connect, before sending any commands.
    * Update nyc to address potential security issues

--- test/aerospike_store_test.js
+++ test/aerospike_store_test.js
@@ -35,16 +35,24 @@ function lifecycleTest (store, t) {
   Promise.promisifyAll(store)
 
   const session = { cookie: { maxAge: 2000 }, name: 'jan' }
-  return store.setAsync('sid', session)
-    .then(() => t.pass('#set() ok'))
-    .then(() => store.getAsync('sid'))
-    .then(data => t.deepEqual({ cookie: { maxAge: 2000 }, name: 'jan' }, data, '#get() ok'))
-    .then(() => store.touchAsync('sid', session))
-    .then(() => t.pass('#touch() ok'))
-    .then(() => store.destroyAsync('sid'))
-    .then(() => store.getAsync('sid'))
-    .then(data => t.equal(undefined, data, '#destroy() ok'))
-    .then(() => store.close(false))
+  return new Promise((resolve) => {
+    store.on('connect', () => resolve(
+      store.setAsync('sid', session)
+        .then(() => t.pass('#set() ok'))
+        .then(() => store.getAsync('sid'))
+        .then(data => t.deepEqual({ cookie: { maxAge: 2000 }, name: 'jan' }, data, '#get() ok'))
+        .then(() => store.touchAsync('sid', session))
+        .then(() => t.pass('#touch() ok'))
+        .then(() => store.destroyAsync('sid'))
+        .then(() => store.getAsync('sid'))
+        .then(data => t.equal(undefined, data, '#destroy() ok'))
+        .then(() => store.close(false))
+        .catch((error) => {
+          store.close(false)
+          t.fail(error)
+        })
+    ))
+  })
 }
 
 test('constructor', function (t) {
@@ -59,8 +67,10 @@ test('defaults', function (t) {
   t.notOk(store.ttl, 'ttl not set')
   t.ok(store.client, 'creates client')
 
-  store.client.close(false)
-  t.end()
+  store.on('connect', () => {
+    store.client.close(false)
+    t.end()
+  })
 })
 
 test('basic', function (t) {
@@ -72,13 +82,17 @@ test('clear', function (t) {
   const store = new AerospikeStore()
   Promise.promisifyAll(store)
 
-  return store.setAsync('sess1', { name: 'jan' })
-    .then(() => delay(5))
-    .then(() => store.clearAsync())
-    .then(() => delay(100))
-    .then(() => store.getAsync('sess1'))
-    .then(session => t.equal(session, undefined, 'all sessions cleared'))
-    .then(() => store.close(false))
+  return new Promise((resolve) => {
+    store.on('connect', () => resolve(
+      store.setAsync('sess1', { name: 'jan' })
+        .then(() => delay(5))
+        .then(() => store.clearAsync())
+        .then(() => delay(100))
+        .then(() => store.getAsync('sess1'))
+        .then(session => t.equal(session, undefined, 'all sessions cleared'))
+        .then(() => store.close(false))
+    ))
+  })
 })
 
 test('existing client', function (t) {
@@ -158,9 +172,11 @@ test('mapper error', function (t) {
   b.a = a
 
   const store = new AerospikeStore()
-  store.set('sid', a, error => {
-    t.assert(error && error.message.match(/circular/), 'serialization error is passed to callback')
-    store.close(false)
-    t.end()
+  store.on('connect', () => {
+    store.set('sid', a, (error) => {
+      t.assert(error && error.message.match(/circular/), 'serialization error is passed to callback')
+      store.close(false)
+      t.end()
+    })
   })
 })
